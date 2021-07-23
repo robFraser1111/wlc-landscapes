0f92652031591eb5989920862ff7031efe82568f
robFraser1111 <robfraserdesign@gmail.com>
2021-07-23 22:56:24 +1000

    Added Form using Form Spree

--- components/contact-form.js
+++ components/contact-form.js
@@ -1,105 +1,99 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import styles from "../styles/ContactForm.module.css";
 
 export default function ContactForm() {
-    const [status, setStatus] = useState({
-        submitted: false,
+  const [status, setStatus] = useState({
+    submitted: false,
+    submitting: false,
+    info: { error: false, msg: null },
+  });
+  const [inputs, setInputs] = useState({
+    email: "",
+    message: "",
+  });
+  const handleServerResponse = (ok, msg) => {
+    if (ok) {
+      setStatus({
+        submitted: true,
         submitting: false,
-        info: { error: false, msg: null }
+        info: { error: false, msg: msg },
+      });
+      setInputs({
+        email: "",
+        message: "",
+      });
+    } else {
+      setStatus({
+        info: { error: true, msg: msg },
+      });
+    }
+  };
+  const handleOnChange = (e) => {
+    e.persist();
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+    setStatus({
+      submitted: false,
+      submitting: false,
+      info: { error: false, msg: null },
+    });
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
+    axios({
+      method: "POST",
+      url: "https://formspree.io/https://formspree.io/f/xgerlbll",
+      data: inputs,
+    })
+      .then((response) => {
+        handleServerResponse(
+          true,
+          "Thank you, your message has been submitted."
+        );
       })
-    
-      const [inputs, setInputs] = useState({
-        email: '',
-        message: ''
-      })
-    
-      const handleResponse = (status, msg) => {
-        if (status === 200) {
-          setStatus({
-            submitted: true,
-            submitting: false,
-            info: { error: false, msg: msg }
-          })
-          setInputs({
-            email: '',
-            message: ''
-          })
-        } else {
-          setStatus({
-            info: { error: true, msg: msg }
-          })
-        }
-      }
-    
-      const handleOnChange = e => {
-        e.persist()
-        setInputs(prev => ({
-          ...prev,
-          [e.target.id]: e.target.value
-        }))
-        setStatus({
-          submitted: false,
-          submitting: false,
-          info: { error: false, msg: null }
-        })
-      }
-    
-      const handleOnSubmit = async e => {
-        e.preventDefault()
-        setStatus(prevStatus => ({ ...prevStatus, submitting: true }))
-        const res = await fetch('/api/send', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(inputs)
-        })
-        const text = await res.text()
-        handleResponse(res.status, text)
-      }
-
-    return (
-
-        <section className={ styles.section }>
-
-        <form onSubmit={handleOnSubmit}>
-            <label htmlFor="email">Email</label>
-            <input
-            id="email"
-            type="email"
-            onChange={handleOnChange}
-            required
-            value={inputs.email}
-            />
-            <label htmlFor="message">Message</label>
-            <textarea
-            id="message"
-            onChange={handleOnChange}
-            required
-            value={inputs.message}
-            />
-            <button type="submit" disabled={status.submitting}>
-            {!status.submitting
-                ? !status.submitted
-                ? 'Submit'
-                : 'Submitted'
-                : 'Submitting...'}
-            </button>
-        </form>
-
-        {status.info.error && (
-            <div className="error">Error: {status.info.msg}</div>
-        )}
-        {!status.info.error && status.info.msg && (
-            <div className="success">{status.info.msg}</div>
-        )}
-
-        </section>
-
-    )
+      .catch((error) => {
+        handleServerResponse(false, error.response.data.error);
+      });
+  };
+  return (
+    <main>
+      <h1>React and Formspree</h1>
+      <hr />
+      <form onSubmit={handleOnSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          name="_replyto"
+          onChange={handleOnChange}
+          required
+          value={inputs.email}
+        />
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          name="message"
+          onChange={handleOnChange}
+          required
+          value={inputs.message}
+        />
+        <button type="submit" disabled={status.submitting}>
+          {!status.submitting
+            ? !status.submitted
+              ? "Submit"
+              : "Submitted"
+            : "Submitting..."}
+        </button>
+      </form>
+      {status.info.error && (
+        <div className="error">Error: {status.info.msg}</div>
+      )}
+      {!status.info.error && status.info.msg && <p>{status.info.msg}</p>}
+    </main>
+  );
 }
-
-
-
-
